06b086716ede5123a552843181d4e3d9bef96b79
Ryan Wan <sc16wyrw@leeds.ac.uk>
2017-05-02 19:59:48 +0100

    Ex13

--- exercises/Ex13.java
+++ exercises/Ex13.java
@@ -13,20 +13,20 @@ public class Ex13
     BankAccount bankAccount1 = new BankAccount(id, name, 20000);
     bankAccount1.withdraw(2500);
     bankAccount1.deposit(3000);
-    System.out.println("\nThe current balance of the account is: " + bankAccount1.getBalance());
+    System.out.println("\nThe current balance of the account is: £" + bankAccount1.getBalance());
     System.out.printf("As a string: %s%n", bankAccount1);
 
     OverdraftAccount overdraftAccount1 = new OverdraftAccount(id, name, 2000);
     overdraftAccount1.setOverdraftLimit(500);
     overdraftAccount1.withdraw(2500);
-    System.out.println("\nThe current balance of the account is: " + overdraftAccount1.getBalance());
+    System.out.println("\nThe current balance of the account is: £" + overdraftAccount1.getBalance());
     System.out.printf("As a string: %s%n", overdraftAccount1);
 
     SavingsAccount savingsAccount1 = new SavingsAccount(id, name, 2000);
     savingsAccount1.setInterestRate(5);
     savingsAccount1.applyInterest();
-    System.out.println("\nThe current balance of the account is: " + savingsAccount1.getBalance());
-    //System.out.printf("As a string: %s%n", savingsAccount1);
+    System.out.println("\nThe current balance of the account is: £" + savingsAccount1.getBalance());
+    System.out.printf("As a string: %s%n", savingsAccount1);
   }
 }
 
@@ -90,7 +90,7 @@ public class Ex13
     @Override
     public String toString()
     {
-      return "ID: " + getID() + ", Name: " + getName() + ", Balance: " + getBalance();
+      return getClass().getName() + ", ID: " + getID() + ", Name: " + getName() + ", Balance: £" + getBalance();
     }
   }
 
@@ -137,7 +137,7 @@ public class Ex13
     @Override
     public String toString()
     {
-      return String.format("Overdraft Account, " + "%s, %s, %d", super.toString(), "Overdraft Limit: ", getOverdraftLimit());
+      return String.format("%s, %s %s%d", super.toString(), "Overdraft Limit:", "£", getOverdraftLimit());
     }
   }
 
@@ -169,12 +169,12 @@ public class Ex13
 
     void applyInterest()
     {
-        balance = (int) (balance + balance *(interestRate/100));
+      balance = (int) (balance + balance *(interestRate/100));
     }
 
     @Override
     public String toString()
     {
-      return String.format("Savings Account, " + "%s, s%, %d", super.toString(), "Interest Rate: ", getInterestRate());
+      return String.format("%s, %s %.2f%s", super.toString(), "Interest Rate:", getInterestRate(), "%");
     }
   }
